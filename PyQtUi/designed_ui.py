2cbfc72731e8953ba5033c6f13c82bc03018687f
Lang Xinming <cemekajustskill04@gmail.com>
2026-01-17 12:59:34 +0800

    尝试将ui文件使用python代码实现

---
+++ PyQtUi/designed_ui.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'designed.ui'
+##
+## Created by: Qt User Interface Compiler version 6.10.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+import sys
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtOpenGLWidgets import QOpenGLWidget
+from PySide6.QtWidgets import (QApplication, QComboBox, QDoubleSpinBox, QHBoxLayout,
+    QLabel, QMainWindow, QMenuBar, QPushButton,
+    QSizePolicy, QStatusBar, QVBoxLayout, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.openGLWidget = QOpenGLWidget(self.centralwidget)
+        self.openGLWidget.setObjectName(u"openGLWidget")
+        self.openGLWidget.setGeometry(QRect(-11, -1, 681, 471))
+        self.verticalLayoutWidget = QWidget(self.centralwidget)
+        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
+        self.verticalLayoutWidget.setGeometry(QRect(670, 0, 131, 80))
+        self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.pushButton = QPushButton(self.verticalLayoutWidget)
+        self.pushButton.setObjectName(u"pushButton")
+
+        self.verticalLayout.addWidget(self.pushButton)
+
+        self.pushButton_2 = QPushButton(self.verticalLayoutWidget)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+
+        self.verticalLayout.addWidget(self.pushButton_2)
+
+        self.verticalLayoutWidget_2 = QWidget(self.centralwidget)
+        self.verticalLayoutWidget_2.setObjectName(u"verticalLayoutWidget_2")
+        self.verticalLayoutWidget_2.setGeometry(QRect(670, 80, 135, 391))
+        self.verticalLayout_2 = QVBoxLayout(self.verticalLayoutWidget_2)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.label = QLabel(self.verticalLayoutWidget_2)
+        self.label.setObjectName(u"label")
+
+        self.verticalLayout_2.addWidget(self.label)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.doubleSpinBox = QDoubleSpinBox(self.verticalLayoutWidget_2)
+        self.doubleSpinBox.setObjectName(u"doubleSpinBox")
+
+        self.horizontalLayout.addWidget(self.doubleSpinBox)
+
+        self.comboBox = QComboBox(self.verticalLayoutWidget_2)
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.setObjectName(u"comboBox")
+
+        self.horizontalLayout.addWidget(self.comboBox)
+
+
+        self.verticalLayout_2.addLayout(self.horizontalLayout)
+
+        self.label_2 = QLabel(self.verticalLayoutWidget_2)
+        self.label_2.setObjectName(u"label_2")
+
+        self.verticalLayout_2.addWidget(self.label_2)
+
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.doubleSpinBox_2 = QDoubleSpinBox(self.verticalLayoutWidget_2)
+        self.doubleSpinBox_2.setObjectName(u"doubleSpinBox_2")
+
+        self.horizontalLayout_2.addWidget(self.doubleSpinBox_2)
+
+        self.comboBox_2 = QComboBox(self.verticalLayoutWidget_2)
+        self.comboBox_2.addItem("")
+        self.comboBox_2.addItem("")
+        self.comboBox_2.setObjectName(u"comboBox_2")
+
+        self.horizontalLayout_2.addWidget(self.comboBox_2)
+
+
+        self.verticalLayout_2.addLayout(self.horizontalLayout_2)
+
+        self.pushButton_3 = QPushButton(self.verticalLayoutWidget_2)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+
+        self.verticalLayout_2.addWidget(self.pushButton_3)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 800, 22))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"\u64ad\u653e", None))
+        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"\u6682\u505c", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"\u6570\u636e\u8bfb\u53d6\u95f4\u9694", None))
+        self.comboBox.setItemText(0, QCoreApplication.translate("MainWindow", u"\u79d2", None))
+        self.comboBox.setItemText(1, QCoreApplication.translate("MainWindow", u"\u6beb\u79d2", None))
+
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"\u7535\u538b\u6700\u5927\u503c", None))
+        self.comboBox_2.setItemText(0, QCoreApplication.translate("MainWindow", u"\u4f0f", None))
+        self.comboBox_2.setItemText(1, QCoreApplication.translate("MainWindow", u"\u6beb\u4f0f", None))
+
+        self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"\u66f4\u65b0\u8bbe\u7f6e", None))
+    # retranslateUi
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
